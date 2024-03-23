7f3f7fdae6934d6c6aa684e00af88bd3f3b29d0c
Romchik38 <pomahehko.c@gmail.com>
2024-03-23 18:06:56 +0200

    tast 2.2.4 in progress

---
+++ tasks/2_2_4_construct_query.sql
@@ -0,0 +1,44 @@
+-- sales_order ( shipping_address_id ) => sales_order_address ( entity_id )
+-- sales_order_address ( quote_address_id ) => quote_address ( address_id )
+-- quote_address ( address_id ) => rm38_checkout_shipping_comment ( quote_address_id )
+-- save_in_address_book = 0
+
+--cardId - 44
+--shippingAddressId - 101
+--comment - 'some'
+--saveInAddressBook - '0'
+
+--orderId - 000000005
+
+--[ #1 ] 
+SELECT entity_id,
+    customer_id,
+    customer_is_guest,
+    quote_address_id,
+    quote_id,
+    shipping_address_id,
+    increment_id
+    FROM sales_order
+        WHERE entity_id = 5;
+--+-----------+-------------+-------------------+------------------+----------+---------------------+--------------+
+--| entity_id | customer_id | customer_is_guest | quote_address_id | quote_id | shipping_address_id | increment_id |
+--+-----------+-------------+-------------------+------------------+----------+---------------------+--------------+
+--|         5 |        NULL |                 1 |             NULL |       44 |                   9 | 000000005    |
+--+-----------+-------------+-------------------+------------------+----------+---------------------+--------------+
+
+
+SELECT entity_id, quote_address_id
+    FROM sales_order_address
+        WHERE entity_id = 9;
+--+-----------+------------------+
+--| entity_id | quote_address_id |
+--+-----------+------------------+
+--|         9 |              101 |
+--+-----------+------------------+
+
+SELECT quote_address.address_id,
+    rm38_checkout_shipping_comment.comment
+    FROM quote_address, rm38_checkout_shipping_comment
+    WHERE 
+        quote_address.address_id = 101 AND
+        quote_address.address_id = rm38_checkout_shipping_comment.quote_address_id;
