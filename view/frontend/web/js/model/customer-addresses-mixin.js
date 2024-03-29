ae2c6c3807f66dacae5635c7d139c22fe75a81d2
Romchik38 <pomahehko.c@gmail.com>
2024-03-29 11:59:28 +0200

    extension attributes shows on storefront correctly

--- view/frontend/web/js/model/customer-addresses-mixin.js
+++ view/frontend/web/js/model/customer-addresses-mixin.js
@@ -4,14 +4,26 @@ define([], function() {
     return origFile => {
 
         var items = origFile.getAddressItems();
+        var addresses = window.customerData.addresses;
 
         for (var item of items) {
-            item.customAttributes.push(
-                { 
-                    attribute_code: "comment_field", 
-                    value: "asdf" 
-                }
-            );
+            var address = addresses[item.customerAddressId];
+            if (!address || typeof address !== 'object') continue;
+            var keys = Object.keys(address);
+            if (keys.indexOf('extension_attributes') === -1) continue;
+
+            var extensionAttributes = address['extension_attributes'];
+            var attributeCodes = Object.keys(extensionAttributes);
+            
+            for (var attrinuteCode of attributeCodes) {
+                item.customAttributes.push(
+                    { 
+                        attribute_code: attrinuteCode, 
+                        value: extensionAttributes[attrinuteCode]
+                    }
+                );
+    
+            }
         }
 
         return {
