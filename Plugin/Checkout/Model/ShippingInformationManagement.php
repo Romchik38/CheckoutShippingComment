7d9ebddeb13167872470f1c788fa04daacc53f2d
Romchik38 <pomahehko.c@gmail.com>
2024-04-01 18:24:12 +0300

    fixes

--- Plugin/Checkout/Model/ShippingInformationManagement.php
+++ Plugin/Checkout/Model/ShippingInformationManagement.php
@@ -6,17 +6,13 @@ namespace Romchik38\CheckoutShippingComment\Plugin\Checkout\Model;
 
 use Magento\Quote\Api\CartRepositoryInterface;
 use Romchik38\CheckoutShippingComment\Model\ShippingCommentRepository;
-use Magento\Framework\Api\SearchCriteriaBuilder;
-use Magento\Framework\Api\FilterFactory;
 use Magento\Framework\Exception\NoSuchEntityException;
 
 class ShippingInformationManagement
 {
     public function __construct(
         private CartRepositoryInterface $quoteRepository,
-        private ShippingCommentRepository $shippingCommentRepository,
-        private SearchCriteriaBuilder   $searchCriteriaBuilder,
-        private FilterFactory           $filterFactory
+        private ShippingCommentRepository $shippingCommentRepository
     ) {
     }
 
