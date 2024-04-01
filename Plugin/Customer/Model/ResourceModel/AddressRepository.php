cd474c5bf46fe072bba69f2acd3fa7df6167b09b
Romchik38 <pomahehko.c@gmail.com>
2024-04-01 18:24:52 +0300

    is done - plugin for AddressRepository

--- Plugin/Customer/Model/ResourceModel/AddressRepository.php
+++ Plugin/Customer/Model/ResourceModel/AddressRepository.php
@@ -6,16 +6,21 @@ namespace Romchik38\CheckoutShippingComment\Plugin\Customer\Model\ResourceModel;
 
 use Romchik38\CheckoutShippingComment\Model\ShippingCommentCustomerRepository;
 use Magento\Framework\Exception\NoSuchEntityException;
+use Magento\Framework\Exception\CouldNotSaveException;
+use Magento\Framework\Exception\CouldNotDeleteException;
+use Magento\Framework\Message\ManagerInterface;
+use Psr\Log\LoggerInterface;
 
 class AddressRepository
 {
 
     public function __construct(
-        private ShippingCommentCustomerRepository $shippingCommentCustomerRepository
+        private ShippingCommentCustomerRepository $shippingCommentCustomerRepository,
+        private ManagerInterface $messageManager,
+        private LoggerInterface $logger
     ) {
     }
 
-    // IN PROGRESS
     /** 
      *  @param int $addressId 
      */
@@ -42,34 +47,39 @@ class AddressRepository
         return $result;
     }
 
-    // MUST BE TESTED
-    public function afterGetList(
-        $subject,
-        // ? check is it true
-        \Magento\Customer\Api\Data\AddressSearchResultsInterface $searchResults
-    ) {
-        $a = 1;
-        $b = $a + 1;
-        return $searchResults;
-    }
+    // public function afterGetList(){}
+    // do not need, because inside getList() used getById():
+    //      $addresses[] = $this->getById($address->getId());
 
-    // MUST BE TESTED
     public function afterSave(
         $subject,
-        // ? check is it true
         \Magento\Customer\Api\Data\AddressInterface $result,
         \Magento\Customer\Api\Data\AddressInterface $address,
     ) {
+        $customerAddressId = $result->getId();
+        $extensionAttributes = $address->getExtensionAttributes();
+        $commentField = $extensionAttributes->getCommentField();
+        // 1. exit if comment wasn't provided
+        if(!$commentField) {
+            return $result;
+        }
+        // 2. get comment
+        try {
+            $comment = $this->shippingCommentCustomerRepository->getByCustomerAddressId($customerAddressId);
+        // 3. save comment
+            try {
+                $comment->setComment($commentField);
+                $this->shippingCommentCustomerRepository->save($comment);
+            } catch(CouldNotSaveException $e) {
+                $this->messageManager->addErrorMessage(__("Error while saving address comment"));
+                $this->logger->critical('Error while saving shipping comment customer with id: ' . $customerAddressId);
+            }
+        } catch(NoSuchEntityException $e) {
+        }
         return $result;
     }
 
-    // MUST BE TESTED
-    public function afterDelete(
-        $subject,
-        // ? check is it true
-        bool $result,
-        \Magento\Customer\Api\Data\AddressInterface $address
-    ) {
-        return $result;
-    }
+    // public function afterDelete() {}
+    // do not need, because there is a on delete cascade option in mysql
+    // row alraedy deleted
 }
