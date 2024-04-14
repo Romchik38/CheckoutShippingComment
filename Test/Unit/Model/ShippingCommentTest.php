e2d9c42d67656f38d48f26806b5aa4f50883e478
Romchik38 <pomahehko.c@gmail.com>
2024-04-14 15:45:00 +0300

    refactor ( for test )

---
+++ Test/Unit/Model/ShippingCommentTest.php
@@ -0,0 +1,40 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Romchik38\CheckoutShippingComment\Test\Unit\Model;
+
+use Romchik38\CheckoutShippingComment\Model\ShippingComment;
+use Romchik38\CheckoutShippingComment\Model\ResourceModel\ShippingComment as ShippingCommentResource;
+use \Magento\Framework\Model\Context;
+use \Magento\Framework\Registry;
+use PHPUnit\Framework\TestCase;
+
+class ShippingCommentTest extends TestCase
+{
+    private $context;
+    private $registry;
+    private $data = [];
+    private $shippingCommentResource;
+
+    public function setUp(): void {
+        $this->context = $this->createMock(Context::class);
+        $this->registry = $this->createMock(Registry::class);
+        $this->shippingCommentResource = $this->createMock(ShippingCommentResource::class);
+        // $this->data = [
+        //     'entity_id' => '1',
+        //     'quote_address_id' => '5',
+        //     'comment' => 'some comment'
+        // ];
+    }
+
+    public function testGetMethods(){
+        $comment = new ShippingComment(
+            $this->context, 
+            $this->registry,
+            $this->shippingCommentResource
+        );
+        $comment->setData('entity_id', '1');
+        $this->assertSame('1', $comment->getData('entity_id'));
+    }
+}
