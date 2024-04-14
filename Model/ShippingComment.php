7513922b512ae1c5be29483f82b8c979b17945e7
Romchik38 <pomahehko.c@gmail.com>
2024-04-14 15:45:41 +0300

    fixes

--- Model/ShippingComment.php
+++ Model/ShippingComment.php
@@ -7,13 +7,24 @@ namespace Romchik38\CheckoutShippingComment\Model;
 use Romchik38\CheckoutShippingComment\Api\Data\ShippingCommentInterface;
 use Magento\Framework\Model\AbstractModel;
 use Romchik38\CheckoutShippingComment\Model\ResourceModel\ShippingComment as ShippingCommentResource;
+use \Magento\Framework\Model\Context;
+use \Magento\Framework\Registry;
 
 class ShippingComment extends AbstractModel implements ShippingCommentInterface
 {
+    public function __construct(
+        \Magento\Framework\Model\Context $context,
+        \Magento\Framework\Registry $registry,
+        private ShippingCommentResource $shippingCommentResource
+    )
+    {
+        parent::__construct($context, $registry);
+    }
+
     protected function _construct()
 
     {
-        $this->_init(ShippingCommentResource::class);
+        $this->_init($this->shippingCommentResource::class);
     }
 
     public function getId()
