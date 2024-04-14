c8cff58c4f31b50f00ec6a8e3f9752ec936f0a70
Romchik38 <pomahehko.c@gmail.com>
2024-04-14 12:35:32 +0300

    test is done - Controller/Customer/Edit

--- Test/Unit/Controller/Customer/EditTest.php
+++ Test/Unit/Controller/Customer/EditTest.php
@@ -52,11 +52,11 @@ class EditTest extends TestCase
             $jsonFactory,
             $session,
             $request,
-            $addressRepositor,
+            $addressRepository,
             $json
         ] = $this->createClasses();
 
-        $controller = new Edit($jsonFactory, $session, $request, $addressRepositor);
+        $controller = new Edit($jsonFactory, $session, $request, $addressRepository);
 
         $jsonFactory->method('create')->willReturn($json);
 
@@ -74,11 +74,11 @@ class EditTest extends TestCase
             $jsonFactory,
             $session,
             $request,
-            $addressRepositor,
+            $addressRepository,
             $json
         ] = $this->createClasses();
 
-        $controller = new Edit($jsonFactory, $session, $request, $addressRepositor);
+        $controller = new Edit($jsonFactory, $session, $request, $addressRepository);
 
         $jsonFactory->method('create')->willReturn($json);
 
@@ -90,6 +90,136 @@ class EditTest extends TestCase
             }));
 
         $request->method('getParam')->willReturn('');
+
+        $controller->execute();
+    }
+
+    public function testExecuteCustomerId()
+    {
+        [
+            $jsonFactory,
+            $session,
+            $request,
+            $addressRepository,
+            $json
+        ] = $this->createClasses();
+
+        $controller = new Edit($jsonFactory, $session, $request, $addressRepository);
+
+        $jsonFactory->method('create')->willReturn($json);
+
+        $json->method('setData')->willReturnSelf();
+        $json->method('setData')->with($this->equalTo(['error' => 'authentication required']));
+
+        $request->method('getParam')->willReturn('100');
+
+        $session->method('getCustomerId')->willReturn(null);
+
+        $controller->execute();
+    }
+
+    public function testExecuteAddressIdNotFound()
+    {
+        [
+            $jsonFactory,
+            $session,
+            $request,
+            $addressRepository,
+            $json
+        ] = $this->createClasses();
+
+        $controller = new Edit($jsonFactory, $session, $request, $addressRepository);
+
+        $jsonFactory->method('create')->willReturn($json);
+
+        $json->method('setData')->willReturnSelf();
+        $json->method('setData')->with($this->equalTo(['error' => 'address with id 46 was not found']));
+
+        $request->method('getParam')->willReturn('46');
+
+        $session->method('getCustomerId')->willReturn('1');
+
+        $addressRepository
+            ->method('getById')
+            ->will($this->throwException(new LocalizedException(__('some'))));
+
+        $controller->execute();
+    }
+
+    public function testExecuteCustomerIdNotEqual()
+    {
+        [
+            $jsonFactory,
+            $session,
+            $request,
+            $addressRepository,
+            $json
+        ] = $this->createClasses();
+
+        $controller = new Edit($jsonFactory, $session, $request, $addressRepository);
+
+        $jsonFactory->method('create')->willReturn($json);
+
+        $json->method('setData')->willReturnSelf();
+        $json->method('setData')->with($this->equalTo(['error' => 'access denied']));
+
+        $request->method('getParam')->willReturn('100');
+
+        $session->method('getCustomerId')->willReturn('1');
+
+        $addressRepository->method('getById')->willReturn(
+            new class()
+            {
+                public function getCustomerId()
+                {
+                    return '2';
+                }
+            }
+        )->with($this->equalTo('100'));
+
+        $controller->execute();
+    }
+
+    public function testExecuteSendData()
+    {
+        [
+            $jsonFactory,
+            $session,
+            $request,
+            $addressRepository,
+            $json
+        ] = $this->createClasses();
+
+        $controller = new Edit($jsonFactory, $session, $request, $addressRepository);
+
+        $jsonFactory->method('create')->willReturn($json);
+
+        $json->method('setData')->willReturnSelf();
+        $json->method('setData')->with($this->equalTo(['data' => 'some comment']));
+
+        $request->method('getParam')->willReturn('100');
+
+        $session->method('getCustomerId')->willReturn('1');
+
+        $addressRepository->method('getById')->willReturn(
+            new class
+            {
+                public function getCustomerId()
+                {
+                    return '1';
+                }
+                public function getExtensionAttributes()
+                {
+                    return new class
+                    {
+                        public function getCommentField()
+                        {
+                            return 'some comment';
+                        }
+                    };
+                }
+            }
+        );
 
         $controller->execute();
     }
