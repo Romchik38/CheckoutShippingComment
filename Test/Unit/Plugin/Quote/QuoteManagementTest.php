ec9fc2d6e2816c44f97db5dda2095f005ed957ac
Romchik38 <pomahehko.c@gmail.com>
2024-04-23 20:39:07 +0300

    test in progress QuoteManagementTest

---
+++ Test/Unit/Plugin/Quote/QuoteManagementTest.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Romchik38\CheckoutShippingComment\Test\Unit\Plugin\Quote;
+
+use Romchik38\CheckoutShippingComment\Plugin\Quote\Model\QuoteManagement as Plugin;
+use Romchik38\CheckoutShippingComment\Model\ShippingCommentRepository;
+use \Psr\Log\LoggerInterface\Proxy;
+use \Magento\Framework\Exception\NoSuchEntityException;
+use \Magento\Quote\Model\QuoteManagement;
+use \Magento\Quote\Model\Quote;
+
+class QuoteManagementTest extends \PHPUnit\Framework\TestCase
+{
+
+}
