5a4de1bfa12856403750ead1bb8f53ffcdd953c8
Romchik38 <pomahehko.c@gmail.com>
2024-04-24 23:35:35 +0300

    test in progress - RendererTest

---
+++ Test/Unit/Plugin/Sales/Model/Order/Address/RendererTest.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Romchik38\CheckoutShippingComment\Test\Unit\Plugin\Sales\Model\Order\Address;
+
+use Romchik38\CheckoutShippingComment\Plugin\Sales\Model\Order\Address\Renderer as Plugin;
+
+
+class RendererTest extends \PHPUnit\Framework\TestCase
+{
+  
+}
